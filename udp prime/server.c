4cbc550b9a58932b77b0a1a723f2cad6949cc173
Manika Elizabeth Roy <118897269+manikalizroy@users.noreply.github.com>
2026-05-17 21:18:12 +0530

    Update server.c

--- udp prime/server.c
+++ udp prime/server.c
@@ -1,65 +1,54 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<string.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 int isprime(int n)
 {
-        if(n<=0)
-        {
-                return 0;
-        }
-        for(int i=2;i*i<=n;i++)
-        {
-                if(n%i==0)
-                        return 0;
-        }
-        return 1;
+   int i;
+   if (n==1)
+       return 0;
+   for(i=2;i<=n/2;i++)
+   {
+       if(n%i == 0)
+           return 0;
+   }
+   return 1;
 }
 
 int main()
 {
-        int sock,n;
-        struct sockaddr_in server_addr,client_addr;
-        socklen_t addr_len=sizeof(client_addr);
-        char buffer[1024];
-        int bytes;
+   int sockfd,n;
+   struct sockaddr_in serv_addr, client_addr;
+   socklen_t len = sizeof(client_addr);
 
-        sock=socket(AF_INET,SOCK_DGRAM,0);
+   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-        server_addr.sin_family=AF_INET;
-        server_addr.sin_port=htons(8080);
-        server_addr.sin_addr.s_addr=INADDR_ANY;
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_port = htons(8228);
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-        bind(sock,(struct sockaddr*)&server_addr,sizeof(server_addr));
-        printf("server waiting for connection\n");
+   bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-        //memset(buffer,0,sizeof(buffer));
-        bytes=recvfrom(sock,&n,sizeof(n),0,(struct sockaddr*)&client_addr,&addr_len);
-        printf("Received number:%d\n",n);
+   recvfrom(sockfd, &n, sizeof(n), 0, (struct sockaddr*)&client_addr, &len);
+   printf("Received number: %d\n",n);
 
-        memset(buffer,0,sizeof(buffer));
+   char buffer[100];
+   memset(buffer, 0, sizeof(buffer));
 
-        if(isprime(n))
-        {
-                sprintf(buffer,"Number is prime.Factors are 1,%d\n",n);
-        }
-        else
-        {
-                sprintf(buffer,"Number is composite.Factors are ");
-                for(int i=1;i<=n;i++)
-                {
-                        if(n%i==0)
-                        {
-                                char temp[20];
-                                sprintf(temp,"%d,",i);
-                                strcat(buffer,temp);
-                        }
-                }
-        }
+   if (isprime(n))
+   {
+       strcpy(buffer, "It is a prime number");
+       sendto(sockfd, buffer, strlen(buffer),  0, (struct sockaddr*)&client_addr, len);
+   }
+   else
+   {
+       strcpy(buffer, "It is a composite number");
+       sendto(sockfd, buffer, strlen(buffer),  0, (struct sockaddr*)&client_addr, len);
+   }
 
-        sendto(sock,buffer,sizeof(buffer)-1,0,(struct sockaddr*)&client_addr,addr_len);
-        close(sock);
-        return 0;
+   printf("%s\n", buffer);
+   close(sockfd);
+   return 0;
 }
+
