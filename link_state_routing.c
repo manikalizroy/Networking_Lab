cff3feb4ab1c09c197f4366fa8b56ce6bd7374cc
Manika Elizabeth Roy <118897269+manikalizroy@users.noreply.github.com>
2026-05-17 18:45:22 +0530

    Update link_state_routing.c

--- link_state_routing.c
+++ link_state_routing.c
@@ -1,22 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
-   int n,i,j,src,v,min,adj[10][10],dist[10],last[10],flag[10];
-   printf("enter the no. of nodes: ");
+   int n,i,j,k,v,adj[10][10],dist[10],last[10],flag[10],src,min;
+   printf("Enter the number of nodes:");
    scanf("%d",&n);
-   printf("enter the distance matrix: ");
+   printf("Enter distance matrix\n");
    for(i=0;i<n;i++)
    {
       for(j=0;j<n;j++)
       {
          scanf("%d",&adj[i][j]);
          if(adj[i][j] == 0 && i!=j)
-            adj[i][j] = 1000;
+         {
+            adj[i][j]=1000;
+         }
       }
    }
-   printf("enter source vertex: ");
+
+   printf("Enter the source vertex:");
    scanf("%d",&src);
 
    for(i=0;i<n;i++)
@@ -26,34 +29,40 @@ int main()
       last[i]=0;
    }
 
-   flag[src]=1;
-   dist[src]=0;
-   for(i=0;i<n;i++)
+   flag[src] = 1;
+   dist[src] = 0;
+   last[src] = src;  // source points to itself
+
+   for(i = 0; i < n; i++)
    {
-       min=1000;
-       for(j=0;j<n;j++)
-       {
-          if(flag[j] == 0 && dist[j] < min)
-          {
-             min = dist[j];
-             v = j;
-          }
-       }
-       flag[v]=1;
-       for(j=0;j<n;j++)
-       {
-          if(flag[j]==0)
-          {
-             if(dist[v]+adj[v][j]<dist[j])
-             {
-                dist[j] = dist[v]+adj[v][j];
+      min = 1000;
+      v = -1;  // initialize v to detect disconnected graph
+      for(j = 0; j < n; j++)
+      {
+         if(flag[j] == 0 && dist[j] < min)
+         {
+            min = dist[j];
+            v = j;
+         }
+      }
+
+      if(v == -1) break;  // remaining nodes unreachable
+      flag[v] = 1;
+      for(j = 0; j < n; j++)
+      {
+         if(flag[j] == 0)
+         {
+            if(dist[v] + adj[v][j] < dist[j])
+            {
+                dist[j] = dist[v] + adj[v][j];
                 last[j] = v;
-             }
-          }
-       }
+            }
+        }
+     }
    }
 
-   printf("========Routing Table=======\n");
+   printf("Routing table\n");
+
    printf("Node\tDist\tNext\n");
    for(i=0;i<n;i++)
    {
