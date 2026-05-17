8fd087b55db8934ac4fa29885f4847469f7fdfec
Manika Elizabeth Roy <118897269+manikalizroy@users.noreply.github.com>
2026-05-17 21:13:04 +0530

    Update server.c

--- tcp bidirectional chat/server.c
+++ tcp bidirectional chat/server.c
@@ -5,42 +5,44 @@
 
 int main()
 {
-   int server_fd,new_sock;
-   struct sockaddr_in address;
-   char msg[1000], buffer[1000];
+    int server_fd, newsock;
+    struct sockaddr_in address;
+    char buffer[1024], msg[1024];
 
-   server_fd = socket(AF_INET, SOCK_STREAM,0);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_port = htons(9090);
+    address.sin_addr.s_addr = INADDR_ANY;
 
-   address.sin_family = AF_INET;
-   address.sin_port = htons(8181);
-   address.sin_addr.s_addr = INADDR_ANY;
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd,3);
 
-   bind(server_fd, (struct sockaddr*)&address, sizeof(address));
-   listen(server_fd,3);
+    printf("Waiting for client..\n");
+    newsock = accept(server_fd, NULL, NULL);
 
-   printf("Server listening..\n");
-   new_sock = accept(server_fd, NULL, NULL);
 
-   while(1)
-   {
-      memset(buffer, 0, sizeof(buffer));
-      read(new_sock, buffer, sizeof(buffer));
+    while(1)
+    {
+        //msg from client
+        memset(buffer, 0, sizeof(buffer));
+        read(newsock, buffer, sizeof(buffer));
 
-      if(strncmp(buffer, "exit", 4)==0)
-        break;
+        if(strncmp(buffer, "exit", 4)==0)
+            break;
+        printf("Client: %s\n",buffer);
 
-      printf("Client: %s", buffer);
+        //msg to client
+        memset(msg, 0, sizeof(msg));
+        printf("You: ");
+        fgets(msg, sizeof(msg),stdin);
+        send(newsock, msg, strlen(msg),0);
 
-      printf("Server: ");
-      fgets(msg, sizeof(msg), stdin);
-      send(new_sock, msg, strlen(msg), 0);
+        if(strncmp(msg, "exit", 4)==0)
+            break;
+    }
 
-      if(strncmp(msg, "exit", 4)==0)
-        break;
-
-   }
-   printf("Chat ended.Server Exiting..\n");
-   close(new_sock);
-   close(server_fd);
-   return 0;
+    printf("Chat ended!!\n");
+    close(newsock);
+    close(server_fd);
+    return 0;
 }
